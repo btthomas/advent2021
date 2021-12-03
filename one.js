8706f1ce819cf7401c826008e98955dc6bd39a96
Blake Thomas <blake.thomas@id.me>
2021-12-02 21:26:35 -0500

    1b

--- one.js
+++ one.js
@@ -1,19 +1,38 @@
 import { readFile } from 'fs/promises';
 
+// 1a
+// async function init() {
+//   const input = await readFile('one.txt', 'utf8');
+//   const data = input.split('\n').map((d) => parseInt(d));
+
+//   let last,
+//     counter = 0;
+
+//   data.forEach((d) => {
+//     if (last) {
+//       if (d > last) {
+//         counter++;
+//       }
+//     }
+//     last = d;
+//   });
+
+//   console.log(counter);
+// }
+
+// 1b
 async function init() {
   const input = await readFile('one.txt', 'utf8');
   const data = input.split('\n').map((d) => parseInt(d));
 
-  let last,
-    counter = 0;
+  let counter = 0;
 
   data.forEach((d, i) => {
-    if (last) {
-      if (d > last) {
+    if (i > 2) {
+      if (d > data[i - 3]) {
         counter++;
       }
     }
-    last = d;
   });
 
   console.log(counter);
