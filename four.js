fe97e4d5cb9e1af7f22e7c67f9e8fa848c37465a
Blake Thomas <blake.thomas@id.me>
2021-12-05 09:27:15 -0500

    4b

--- four.js
+++ four.js
@@ -11,6 +11,84 @@ function isSuperset(set, subset) {
   return true;
 }
 
+// 4a
+// async function init() {
+//   const input = await readFile('four.txt', 'utf8');
+
+//   const [rawNumbers, ...rawBoards] = input.split('\n');
+
+//   const numbers = rawNumbers.split(',').map((d) => parseInt(d));
+
+//   const boards = [];
+//   let nextBoard = [];
+
+//   rawBoards.forEach((rawRow, index) => {
+//     if (rawRow === EMPTY_ROW) {
+//       if (index !== 0) {
+//         boards.push(nextBoard);
+//       }
+//       nextBoard = [];
+//     } else {
+//       const row = rawRow
+//         .split(' ')
+//         .filter((d) => d)
+//         .map((d) => parseInt(d));
+
+//       nextBoard.push(row);
+//     }
+//   });
+//   boards.push(nextBoard);
+
+//   const boardSets = boards.map((b) => {
+//     // rows
+//     const sets = b.map((b) => new Set(b));
+//     // cols
+//     for (let i = 0; i < 5; i++) {
+//       sets.push(new Set([b[0][i], b[1][i], b[2][i], b[3][i], b[4][i]]));
+//     }
+//     return sets;
+//   });
+
+//   let numIndex = 0;
+//   const pSet = new Set();
+//   let found = -1;
+//   let last;
+
+//   while (numIndex < numbers.length) {
+//     last = numbers[numIndex];
+//     pSet.add(last);
+
+//     boardSets.forEach((sets, boardI) => {
+//       sets.forEach((set) => {
+//         if (isSuperset(pSet, set)) {
+//           found = boardI;
+//         }
+//       });
+//     });
+
+//     if (found === -1) {
+//       numIndex++;
+//     } else {
+//       break;
+//     }
+//   }
+
+//   console.log(pSet);
+//   const winning = boards[found].flat();
+//   console.log(winning);
+
+//   const sum = winning.reduce((acc, d) => {
+//     if (!pSet.has(d)) {
+//       acc += d;
+//     }
+//     return acc;
+//   }, 0);
+
+//   console.log(sum, last, sum * last);
+// }
+
+// 4b
+
 async function init() {
   const input = await readFile('four.txt', 'utf8');
 
@@ -49,8 +127,8 @@ async function init() {
   });
 
   let numIndex = 0;
-  const pSet = new Set();
-  let found = -1;
+  let pSet = new Set();
+  let found = boards.map(() => false);
   let last;
 
   while (numIndex < numbers.length) {
@@ -58,23 +136,29 @@ async function init() {
     pSet.add(last);
 
     boardSets.forEach((sets, boardI) => {
-      sets.forEach((set) => {
-        if (isSuperset(pSet, set)) {
-          found = boardI;
-        }
-      });
+      if (!found[boardI]) {
+        sets.forEach((set) => {
+          if (isSuperset(pSet, set)) {
+            found[boardI] = numIndex;
+          }
+        });
+      }
     });
 
-    if (found === -1) {
-      numIndex++;
-    } else {
-      break;
-    }
+    numIndex++;
   }
 
-  console.log(pSet);
-  const winning = boards[found].flat();
-  console.log(winning);
+  const num = Math.max(...found);
+  const boardI = found.indexOf(num);
+  const picked = numbers.slice(0, num + 1);
+  pSet = new Set(picked);
+  last = picked[num];
+
+  // console.log(num, boardI);
+  // console.log(picked, last);
+
+  const winning = boards[boardI].flat();
+  // console.log(winning);
 
   const sum = winning.reduce((acc, d) => {
     if (!pSet.has(d)) {
