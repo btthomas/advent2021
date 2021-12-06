0b6dbd0b6f2798d3e2cb123affe12ef9aa3028a4
Blake Thomas <blake.thomas@id.me>
2021-12-06 08:47:45 -0500

    5b

--- five.js
+++ five.js
@@ -1,5 +1,88 @@
 import { readFile } from 'fs/promises';
 
+// 5a
+// async function init() {
+//   const BOARD = [];
+//   const LINES = [];
+
+//   const input = await readFile('five.txt', 'utf8');
+//   const rows = input.split('\n');
+
+//   let max = 0;
+//   rows.forEach((row) => {
+//     const [left, right] = row.split(' -> ');
+//     let [x1, y1] = left.split(',').map((d) => parseInt(d));
+//     let [x2, y2] = right.split(',').map((d) => parseInt(d));
+
+//     if (x1 > max) {
+//       max = x1;
+//     }
+//     if (x2 > max) {
+//       max = x2;
+//     }
+//     if (y1 > max) {
+//       max = y1;
+//     }
+//     if (y2 > max) {
+//       max = y2;
+//     }
+
+//     LINES.push({
+//       x1,
+//       y1,
+//       x2,
+//       y2,
+//     });
+//   });
+
+//   for (let i = 0; i < max + 1; i++) {
+//     BOARD.push([]);
+//     for (let j = 0; j < max + 1; j++) {
+//       BOARD[i][j] = 0;
+//     }
+//   }
+
+//   LINES.forEach(({ x1, y1, x2, y2 }) => {
+//     const isH = y1 === y2;
+//     const isV = x1 === x2;
+
+//     if (isH) {
+//       if (x1 > x2) {
+//         let swap = x1;
+//         x1 = x2;
+//         x2 = swap;
+//       }
+
+//       for (let xx = x1; xx <= x2; xx++) {
+//         BOARD[xx][y1] += 1;
+//       }
+//     }
+//     if (isV) {
+//       if (y1 > y2) {
+//         let swap = y1;
+//         y1 = y2;
+//         y2 = swap;
+//       }
+
+//       for (let yy = y1; yy <= y2; yy++) {
+//         BOARD[x1][yy] += 1;
+//       }
+//     }
+//   });
+
+//   let counter = 0;
+//   BOARD.forEach((row) => {
+//     row.forEach((cell) => {
+//       if (cell > 1) {
+//         counter++;
+//       }
+//     });
+//   });
+//   // console.log(BOARD.join('\n'));
+//   console.log(counter);
+// }
+
+// 5b
 async function init() {
   const BOARD = [];
   const LINES = [];
@@ -55,8 +138,7 @@ async function init() {
       for (let xx = x1; xx <= x2; xx++) {
         BOARD[xx][y1] += 1;
       }
-    }
-    if (isV) {
+    } else if (isV) {
       if (y1 > y2) {
         let swap = y1;
         y1 = y2;
@@ -65,6 +147,23 @@ async function init() {
 
       for (let yy = y1; yy <= y2; yy++) {
         BOARD[x1][yy] += 1;
+      }
+    } else {
+      // diagonal
+      if (x1 > x2) {
+        let swapX = x1;
+        let swapY = y1;
+        x1 = x2;
+        y1 = y2;
+        x2 = swapX;
+        y2 = swapY;
+      }
+
+      const dY = y1 < y2 ? 1 : -1;
+      let yy = y1;
+      for (let xx = x1; xx <= x2; xx++) {
+        BOARD[xx][yy] += 1;
+        yy += dY;
       }
     }
   });
