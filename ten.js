516ea2e53b5e7448cdafc27c499604071084b922
Blake Thomas <blake.thomas@id.me>
2021-12-10 08:31:40 -0500

    10b

--- ten.js
+++ ten.js
@@ -19,15 +19,24 @@ const CLOSERS = {
   },
 };
 
+const LEFTOVER = {
+  '(': 1,
+  '[': 2,
+  '{': 3,
+  '<': 4,
+};
+
 async function init() {
-  const input = await readFile('ten.test.txt', 'utf8');
+  const input = await readFile('ten.txt', 'utf8');
 
   const rows = input.split('\n');
 
   let score = 0;
+  const scores = [];
 
   rows.forEach((row) => {
     const stack = [];
+    let skip = false;
 
     for (let i = 0; i < row.length; i++) {
       const d = CLOSERS[row[i]];
@@ -35,14 +44,25 @@ async function init() {
         const top = stack.pop();
         if (top !== d.char) {
           score += d.value;
+          skip = true;
           break;
         }
       } else {
         stack.push(row[i]);
       }
     }
+
+    if (!skip && stack.length) {
+      const subScore = stack.reduceRight((acc, d) => {
+        return acc * 5 + LEFTOVER[d];
+      }, 0);
+      scores.push(subScore);
+    }
   });
-  console.log(score);
+  console.log({ score });
+
+  scores.sort((a, b) => a - b);
+  console.log({ b: scores[(scores.length - 1) / 2] });
 }
 
 init();
