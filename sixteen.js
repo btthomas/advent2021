ef0aa445eb6537310f7df1eba4714c94c9b240a5
Blake Thomas <blake.thomas@id.me>
2021-12-20 09:40:26 -0500

    16

--- sixteen.js
+++ sixteen.js
@@ -19,112 +19,106 @@ const HEX = {
   F: '1111',
 };
 
+let VVersion = 0;
+
 // 16a
 async function init() {
-  const input = await readFile('sixteen.test.txt', 'utf8');
+  const input = await readFile('sixteen.txt', 'utf8');
 
   const bin = [...input].map((char) => HEX[char]).join('');
 
-  const [result, length] = parseBin({ bin });
-  console.log(result, length);
+  const { result } = parseBin(bin);
+  console.log(VVersion);
+  console.log(result);
 }
 
-function parseBin({ bin, bitLimit, packetLimit }) {
+function parseBin(bin) {
   const version = parseInt(bin.substring(0, 3), 2);
+  VVersion += version;
   const type = parseInt(bin.substring(3, 6), 2);
-  let payload;
-  if (bitLimit || bitLimit === 0) {
-    payload = bin.substring(6, bitLimit);
-  } else {
-    payload = bin.substring(6);
-  }
+  const payload = bin.substring(6);
 
-  console.log({ version, type, payload, bitLimit, packetLimit });
-  let value;
-  let length;
-  let rest;
+  // console.log({ version, type, payload });
 
   if (type === 4) {
     // literal
-    ({ value, length, rest } = parseLiteral(payload));
+    const { result, rest } = parseLiteral(payload);
 
-    if (bitLimit > 0) {
-      const [v, l] = parseBin({ bin: rest, bitLimit: bitLimit - length });
-      return [version + v, length + l];
-    } else if (bitLimit <= 0) {
-      return [version, length];
-    } else if (packetLimit > 1) {
-      const [v, l] = parseBin({ bin: rest, packetLimit: packetLimit - 1 });
-      return [version + v, length + l];
-    } else if (packetLimit <= 1) {
-      return [version, length];
-    } else if (rest.length) {
-      const [v, l] = parseBin({ bin: rest });
-      return [version + v, length + l];
-    } else {
-      return [version, length];
-    }
+    return { result, data: rest };
   } else {
     // operator
     const lenId = payload.substring(0, 1);
     const packetData = payload.substring(1);
+    let results = [];
+    let leftoverData;
+
     if (lenId === '0') {
       // 15 bits
       const totalLength = parseInt(packetData.substring(0, 15), 2);
-      const data = packetData.substring(15, 15 + totalLength);
-      const rest = packetData.substring(15 + totalLength);
+      let data = packetData.substring(15, 15 + totalLength);
+      let result;
 
-      if (bitLimit > 0) {
-        const [v1, l1] = parseBin({ bin: data });
-        const [v2, l2] = parseBin({
-          bin: rest,
-          bitLimit: bitLimit - (15 + totalLength + l1),
-        });
-        return [version + v1 + v2, length + l1 + l2];
-      } else if (bitLimit <= 0) {
-        const [v, l] = parseBin({ bin: data });
-        return [version + v, length + l];
-      } else if (packetLimit > 1) {
-        const [v1, l1] = parseBin({ bin: data });
-        const [v2, l2] = parseBin({ bin: rest, packetLimit: packetLimit - 1 });
-        return [version + v1 + v2, length + l1 + l2];
-      } else if (packetLimit <= 1) {
-        const [v, l] = parseBin({ bin: data });
-        return [version + v, length + l];
-      } else if (rest.length) {
-        const [v1, l1] = parseBin({ bin: data });
-        const [v2, l2] = parseBin({ bin: rest });
-        return [version + v1 + v2, length + l1 + l2];
-      } else {
-        const [v, l] = parseBin({ bin: data });
-        return [version + v, length + l];
+      while (data && data.length) {
+        ({ result, data } = parseBin(data));
+        results.push(result);
       }
+
+      leftoverData = data;
     } else {
       // 11 bits
       const numPackets = parseInt(packetData.substring(0, 11), 2);
-      const data = packetData.substring(11);
+      let data = packetData.substring(11);
+      let result;
 
-      if (bitLimit > 0) {
-        const [v, l] = parseBin({ bin: data, packetLimit: numPackets });
-        return [version + v, l];
-      } else if (bitLimit <= 0) {
-        const [v, l] = parseBin({ bin: data, packetLimit: numPackets });
-        return [version + v, l];
-      } else if (packetLimit > 1) {
-        const [v1, l1] = parseBin({ bin: data, packetLimit: numPackets });
-        const [v2, l2] = parseBin({
-          bin: rest,
-          packetLimit: packetLimit - 1,
-        });
-
-        return [version + v1 + v2, length + l1 + l2];
-      } else if (packetLimit <= 1) {
-        const [v, l] = parseBin({ bin: data, packetLimit: numPackets });
-        return [version + v, l];
-      } else {
-        const [v, l] = parseBin({ bin: data, packetLimit: numPackets });
-        return [version + v, l];
+      for (let i = 0; i < numPackets; i++) {
+        ({ result, data } = parseBin(data));
+        results.push(result);
       }
+
+      leftoverData = data;
+    }
+
+    switch (type) {
+      case 0:
+        return {
+          result: results.reduce((acc, d) => acc + d, 0),
+          data: leftoverData,
+        };
+      case 1:
+        return {
+          result: results.reduce((acc, d) => acc * d, 1),
+          data: leftoverData,
+        };
+      case 2:
+        return {
+          result: Math.min(...results),
+          data: leftoverData,
+        };
+      case 3:
+        return {
+          result: Math.max(...results),
+          data: leftoverData,
+        };
+      case 5:
+        return {
+          result: results[0] > results[1] ? 1 : 0,
+          data: leftoverData,
+        };
+      case 6:
+        return {
+          result: results[0] < results[1] ? 1 : 0,
+          data: leftoverData,
+        };
+      case 7:
+        return {
+          result: results[0] === results[1] ? 1 : 0,
+          data: leftoverData,
+        };
+      default:
+        return {
+          result: 0,
+          data: '',
+        };
     }
   }
 }
@@ -151,12 +145,12 @@ function parseLiteral(payload) {
   }
 
   const rest = payload.substring(index);
-  const value = parseInt(num.join(''), 2);
+  const result = parseInt(num.join(''), 2);
 
-  // console.log({ value });
+  // console.log({ result });
 
   return {
-    value,
+    result,
     length: index,
     rest,
   };
